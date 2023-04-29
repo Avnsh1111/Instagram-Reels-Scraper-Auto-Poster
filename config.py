27789e98b03e2db6bdcdfaf989161535fe1d4a9f
Avnsh1111 <er.avinashrathod@gmail.com>
2023-04-29 18:23:14 +0530

    Added COde

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+#Config Variables
+CURRENT_DIR = os.getcwd() + os.sep
+DB_PATH = CURRENT_DIR + 'database' + os.sep + 'sqlite.db'
+DOWNLOAD_DIR = CURRENT_DIR + 'downloads' + os.sep #Path of folder where files will be stored
+FETCH_LIMIT = 10
+POSTING_INTERVAL_IN_MIN = 15
+SCRAPER_INTERVAL_IN_MIN = 720 #Every 12 hours
+USERNAME = "your_username"
+PASSWORD = "your_password"
+ACCOUNTS = [
+    "totalgaming_official",
+    "carryminati",
+    "techno_gamerz",
+    "payalgamingg",
+    "dynamo__gaming"
+]
+
+HASHTAGS = "#gaming #gamer #ps #playstation #videogames #game #xbox #games #twitch #fortnite #pc #memes #pcgaming #gamers #gamingcommunity #youtube #xboxone #gamergirl #nintendo #gta #callofduty #streamer #follow #pubg #videogame #esports #bhfyp #meme #twitchstreamer #art"
---
+++ poster.py
@@ -0,0 +1,52 @@
+import os
+from instagrapi import Client
+from db import Session, Reel, ReelEncoder
+from datetime import datetime
+import config
+import time
+
+session = Session()
+
+
+# Update is_posted and posted_at field in DB
+def update_status(code):
+    session.query(Reel).filter_by(code=code).update({'is_posted': True, 'posted_at': datetime.now()})
+    session.commit()
+
+
+# Get Unposted reels from database
+def get_reel():
+    reel = session.query(Reel).filter_by(is_posted=False).first()
+    return reel
+
+
+# Magic Starts Here
+def main():
+    api = Client()
+    try:
+        api.login(config.USERNAME, config.PASSWORD)
+        reel = get_reel()
+        if os.path.exists(reel.file_path):
+            media = api.clip_upload(
+                reel.file_path,
+                "Repost From Author : @" + reel.account + " , Caption : " + reel.caption + " " + config.HASHTAGS,
+                extra_data={
+                    # "custom_accessibility_caption": "alt text example",
+                    "like_and_view_counts_disabled": 1,
+                    "disable_comments": 1,
+                })
+
+            if media:
+                update_status(reel.code)
+                time.sleep(config.POSTING_INTERVAL_IN_MIN * 60)
+            main()
+            pass
+
+    except Exception as e:
+        print(f"Exception {type(e).__name__}: {str(e)}")
+        exit()
+        pass
+
+if __name__ == "__main__":
+    main()
+    session.close()
